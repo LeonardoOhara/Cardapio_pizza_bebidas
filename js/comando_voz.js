475a237caf3108028accefa6b23c509051851415
Leonardo Ohara <148023029+LeonardoOhara@users.noreply.github.com>
2025-02-05 17:15:50 -0300

    update_menu
    adicionei uma interface melhor na entrada e um botão para voltar e melhorei um pouco o reconhecimento de voz.

--- js/comando_voz.js
+++ js/comando_voz.js
@@ -1,250 +1,4 @@
-const product = [
 
-     {
-        name: "Pecorina",
-        image: "./assets/pecorina.png",
-        description: "Creme de Pecorino, Nduja, pecorino ralado e mussarela."
-    },
-
-    {
-        name: "Chutney essa manga",
-        image: "./assets/chutney.png",
-        description: "Presunto de Parma italiano, chutney de manga, lascas de parmesão e mussarela."
-    },
-
-     {
-        name: "Funghetti vegana",
-        image: "./assets/funghett.png",
-        description: "Funghi artesanal, parmesão vegetal, azeite trufado e mussarela vegetal."
-    },
-
-    {
-        name: "Bassanina Vegana",
-        image: "./assets/bassanina.png",
-        description: "Aspargos brancos, abobrinha laminada, gorgozola vegetal e mussarela vegetal."
-    },
-
-     {
-        name: "Carciofi (vegana)",
-        image: "./assets/carciofi.png",
-        description: "Coraçao de alcachofra, tomatinhos confitados, parmesão vegetal e mussarela vegetal."
-    },
-
-     {
-        name: "Gianna",
-        image: "./assets/gianna.png",
-        description: "Mussarela, berinjela grelhada, parmesão, tomate assado no forno a lenha e manjericão."
-    },
-
-    {
-        name: "Nduja",
-        image: "./assets/nduja.png",
-        description: "Mussarela de búfala, Nduja, salaminho picante e melaço de cana."
-    },
-
-     {
-        name: "alto",
-        image: "./assets/altopiano.png",
-        description: "Queijo Fontina, funghi artesanal e mussarela"
-    },
-     {
-        name: "Abobrinha",
-        image: "./assets/abobrinha.jpg",
-        description: "Mussarela, calabresa, cebola, pimenta, picles de cebola, cebolinha, tomate, orégano e azeitona."
-    },
-
-    {
-        name: "Atum",
-        image: "./assets/atum.jpg",
-        description: "Atum e Cebola."
-    },
-
-    {
-        name: "Bruta",
-        image: "./assets/bruta.jpg",
-        description: "Queijo Emmental, Pancetta Defumada, Bacon crocante e Mussarela"
-    },
-
-     {
-        name: "Bella marinara",
-        image: "./assets/bella.jpg",
-        description: "Muito molho de tomate, alho fresco laminado, tomatinhos confitados e azeite trufado."
-    },
-
-   {
-        name: "Calabresa",
-        image: "./assets/calabresa.jpg",
-        description: "Calabresa fatiada e cebolas. Roxa e Branca"
-    },
-
-     {
-        name: "Calapiry",
-        image: "./assets/calapiry.jpg",
-        description: "Calabresa moída ou fatiada, catupiry original"
-    },
-
-    {
-        name: "Celina",
-        image: "./assets/celina.png",
-        description: "Queijo Fontina, cebola caramelizada, pancetta e mussarela"
-    },
-
-     {
-        name: "Contadina",
-        image: "./assets/contadina.jpg",
-        description: "Abobrinha fatiada empanada na farinha de Semola ,tomate cereja, mussarela e parmesão"
-    },
-
-     {
-        name: "Di capra",
-        image: "./assets/dicapra.jpg",
-        description: "Queijo de cabra Boursin, tomate assado e nosso molho pesto de manjericão."
-    },
-
-     {
-        name: "Diavola e Zucchine",
-        image: "./assets/diavolaezucchine.jpg",
-        description: "Salaminho picante, abobrinha e mussarela."
-    },
-
-     {
-        name: "Diavola",
-        image: "./assets/diavola.jpg",
-        description: "Salaminho picante e mussarela."
-    },
-
-     {
-        name: "Eliete",
-        image: "./assets/eliete.jpg",
-        description: "Coração de alcachofra e mussarela."
-    },
-
-     {
-        name: "Frango com catupiry",
-        image: "./assets/frangocomcatupiry.jpg",
-        description: "Frango desfiado e requeijão tipo catupiry."
-    },
-
-   {
-        name: "Limonata",
-        image: "./assets/limonata.jpg",
-        description: "Tomatinhos confitados, ricota de búfala, raspas de limão siciliano e mussarela."
-    },
-
-    {
-        name: "Marguerita",
-        image: "./assets/marguerita.jpg",
-        description: "Parmesão, tomate, manjericão e mussarela."
-    },
-
-     {
-        name: "Marguerita Italiana",
-        image: "./assets/margueritaitaliana.jpg",
-        description: "Mussarela de búfala e manjericão."
-    },
-
-    {
-        name: "Montanara",
-        image: "./assets/montanara.jpg",
-        description: "Pancetta, gorgonzola, nozes e mussarela."
-    },
-
-    {
-        name: "Mussarela",
-        image: "./assets/mussarela.jpg",
-        description: "Mussarela."
-    },
-
-    {
-        name: "Nostrana",
-        image: "./assets/nostrana.jpg",
-        description: "Linguiça no forno a lenha, funghi artesanal, parmesão e mussarela."
-    },
-
-     {
-        name: "Parma e Rúcula",
-        image: "./assets/parmaerucula.jpg",
-        description: "Mussarela, queijo stracchino, presunto Parma italiano e rúcula."
-    },
-
-     {
-        name: "Parma e brie",
-        image: "./assets/parmaebrie.jpg",
-        description: "Mussarela de búfala, presunto Parma italiano e brie."
-    },
-
-    {
-        name: "Portuguesa",
-        image: "./assets/portuguesa.jpg",
-        description: "Presunto, ovos cozidos, ervilhas, cebola e mussarela."
-    },
-
-    {
-        name: "Palmito",
-        image: "./assets/palmito.jpg",
-        description: "Palmito, cebola e mussarela."
-    },
-
-     {
-        name: "artesanal",
-        image: "./assets/panna.jpg",
-        description: "Panna, Presunto Parma italiano, azeite trufado e mussarela"
-    },
-
-     {
-        name: "Puttanesca",
-        image: "./assets/puttanesca.jpg",
-        description: "Tomate cereja, cebola roxa, ricota de búfala, mussarela e azeitonas picadas."
-    },
-
-    {
-        name: "Principessa",
-        image: "./assets/principessa.jpg",
-        description: "Queijo de cabra Boursin, nossa abobrinha marinada e folhas de hortelã."
-    },
-
-   {
-        name: "Portuguesa",
-        image: "./assets/portuguesa.jpg",
-        description: "Presunto, ovos cozidos, ervilhas, cebola e mussarela."
-    },
-
-    {
-        name: "Quatro queijos",
-        image: "./assets/quatroqueijos.jpg",
-        description: "Emmental, provolone, gorgonzola e mussarela."
-    },
-
-    {
-        name: "Renata",
-        image: "./assets/renata.jpg",
-        description: "Nossa abobrinha marinada, linguiça no forno a lenha, gorgonzola e mussarela."
-    },
-
-     {
-        name: "Rosmarina",
-        image: "./assets/rosmarina.jpg",
-        description: "Linguiça despedaça, tomate cereja, alecrim e mussarela."
-    },
-
-    {
-        name: "Toscana moída",
-        image: "./assets/toscanamoida.jpg",
-        description: "Calabresa moída, cebola e mussarela."
-    },
-
-    {
-        name: "Toscana fatiada",
-        image: "./assets/toscanamoida.jpg",
-        description: "Calabresa moída, cebola e mussarela."
-    },
-
-    {
-        name: "Tia mania",
-        image: "./assets/tiamania.jpg",
-        description: "Parmesão, alho frito, bacon crocante, tomate e mussarela."
-    },
-]
 
 // Referências aos elementos da página
 const searchInput = document.getElementById('search');
@@ -278,12 +32,14 @@ function closePopup() {
 // Fechar o pop-up ao clicar no botão de fechar
 popupClose.onclick = closePopup;
 
+
+
 // Abrir pop-up com comando de voz
 if ('webkitSpeechRecognition' in window) {
     const recognition = new webkitSpeechRecognition();
-    recognition.lang = 'pt-BR' , 'it-IT'; // Define o idioma para português
+    recognition.lang = 'pt-BR'; // Define o idioma para português
     recognition.interimResults = true; // Permite resultados parciais enquanto fala
-    recognition.maxAlternatives = 2;
+    recognition.maxAlternatives = 1;
     recognition.continuous = true; // Permite que o reconhecimento continue ouvindo
 
      // Variável para controlar o estado da gravação
@@ -295,17 +51,37 @@ if ('webkitSpeechRecognition' in window) {
         const transcript = event.results[event.results.length - 1][0].transcript.toLowerCase(); // Pega a última transcrição
         console.log("Você disse:", transcript);
         
-        // Verifica se a transcrição inclui o nome do produto
+        // Verifica se a transcrição inclui algum nome ou item da lista 'voice' do produto
         for (let key in products) {
-            if (transcript.includes(products[key].name.toLowerCase())) {
-                showProductInPopup(products[key]);
+            const product = products[key];
+            
+            // Verifica se o nome do produto está na transcrição
+            if (product.name && transcript.includes(product.name.toLowerCase())) {
+                showProductInPopup(product);
                 break;
+            }
+            
+            // Verifica se algum dos itens da lista 'voice' está na transcrição
+            if (product.voice && Array.isArray(product.voice)) {
+                for (let voiceItem of product.voice) {
+                    if (transcript.includes(voiceItem.toLowerCase())) {
+                        showProductInPopup(product);
+                        break;
+                    }
+                }
             }
         }
     };
+    
 
     // Iniciar ou parar o reconhecimento de voz ao clicar no botão
     voiceButton.onclick = function() {
+
+        if (products.length === 0) {
+            console.error("Os produtos ainda não foram carregados.");
+            return;
+        }
+
         if (isRecognizing) {
             recognition.stop(); // Para o reconhecimento se estiver em andamento
             voiceButton.textContent = "Clique para Falar"; // Muda o texto do botão
@@ -317,9 +93,10 @@ if ('webkitSpeechRecognition' in window) {
         }
     
     };
-} else {
+    } 
+    else {
     alert("A funcionalidade de comando de voz não é suportada neste navegador.");
-}
+    }
 
 // Exemplo para pesquisa automática ao digitar
 searchInput.addEventListener('input', function() {
@@ -344,18 +121,5 @@ function normalizeString(str) {
         .trim(); // Remove espaços extras
 }
 
-recognition.onresult = function(event) {
-    const transcript = normalizeString(event.results[0][0].transcript);
-    console.log("Você disse:", transcript);
-    
-    for (let key in products) {
-        let normalizedKey = normalizeString(key);
-        let normalizedName = normalizeString(products[key].name);
 
-        if (transcript.includes(normalizedKey) || transcript.includes(normalizedName)) {
-            console.log("Pizza reconhecida:", products[key].name);
-            displayProduct(key);
-            break;
-        }
-    }
-};
+
